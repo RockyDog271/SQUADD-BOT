377e81718d406dbdf8dd1426cc0619abf72acfd3
Logan Simila <Lsimila@ivytech.edu>
2026-05-12 03:13:27 -0400

    Added functionallity notation

---
+++ Functions/FunctionCommands.py
@@ -0,0 +1,19 @@
+# IF msg = >> saves
+
+    # read save amt from json
+
+# IF msg = >> report
+
+    # process report and send to channels
+
+# IF msg = >> ticket
+
+    # process ticket and send to channels
+
+# IF msg = >> suggest
+
+    # process suggestion and send to channels
+
+# ELSE
+
+    # send unknown command error
--- Functions/FunctionModeration.py
+++ Functions/FunctionModeration.py
@@ -1,17 +1,86 @@
 async def BlacklistCheck(DiscordMessage, ContentCheck, LoggingChannelID, ModerationActionPreformed, Client, BlacklistedWord):
-    BlacklistedWord = False
-    if BlacklistedWord == True:
+    Blacklisted = False
+    Blacklisted = LinkBlacklistCheck()
+    
+    with open("blacklist.txt", "r", encoding = "utf-8") as Blacklist:
+        for line in Blacklist:
+            BlacklistWord = line.strip().lower().replace("'", "")
+            if BlacklistWord in ContentCheck:
+                Blacklisted = True
+                break
+            
+
+
+    # for word in blacklist, check if ContentCheck contains blacklisted word
+
+
+    
+    
+    
+    
+    
+    if Blacklisted == True:
         await DiscordMessage.delete()
         await DiscordMessage.channel.send(f"Message from {DiscordMessage.author.mention} deleted for containing a blacklisted word")
         await DiscordMessage.channel.send(f" {ModerationActionPreformed}")
         LoggingChannel = Client.get_channel(LoggingChannelID)
         await LoggingChannel.send(f"Deleted message from {DiscordMessage.author.mention} for containing a blacklisted word. Message content: {DiscordMessage.content}")
-        return (BlacklistedWord)
+        return (Blacklisted)
     else:
-        return (BlacklistedWord)
-    
+        return (Blacklisted)
+
+async def LinkBlacklistCheck():
+    return
+
 async def AutoRoleHierarchy():
     return
 
 async def ChannelActions():
     return
+
+# IF music
+
+    # IF contains link
+
+        # checks if valid link to music site
+
+        # checks if link is blacklisted
+
+        # IF blacklisted
+
+            # moderate
+
+# IF weblinks
+
+    # IF contains link
+
+        # checks if valid link
+
+        # IF invalid
+
+            # moderate
+
+        # checks if link is blacklisted
+
+        # IF blacklisted
+
+            # moderate
+
+
+# IF shareclips
+
+    # IF contains link
+
+        # checks for "clip"
+
+        # IF missing "clip"
+
+            # deletes message and moderation message
+
+        # checks if link is blacklisted
+
+        # IF blacklisted
+
+            # moderate
+
+            
