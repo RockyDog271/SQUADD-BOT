b9f9214785060cbab5c2ed3165eed15ebc6a3986
Logan Simila <Lsimila@ivytech.edu>
2026-05-12 16:20:32 -0400

    Forgot to push this outline

---
+++ Functions/FunctionCounting.py
@@ -0,0 +1,35 @@
+
+
+# Checks the message for just the number
+
+# grabs the last number before the current one
+
+# compares the numbers
+
+# IF number is correct
+
+    # adds check reaction to message
+
+    # if numbercheck in json is ready to add new save
+
+        # if save in json is at max saves
+
+            # return
+
+        # else 
+
+            # add save in json
+
+# ELIF number is wrong
+
+    # adds X reaction to message
+
+    # subtracts saves from json
+    
+    # is json at 0 saves
+
+    # send loss and reset message
+
+# ELSE
+
+    # returns error message, sends to logs not to public
